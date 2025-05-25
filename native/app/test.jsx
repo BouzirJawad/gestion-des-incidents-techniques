bb25e77e889b4067d3d494a9b303ada708385ec4
BouzirJAwad <jawadbouzir01@gmail.com>
2025-05-25 13:36:02 +0100

    add jsx path so reactnative and nativewind reconize jsx

---
+++ native/app/test.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Test() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-600 text-4xl">hello world</Text>
+      <Text className="text-2xl">daba siro l app/index.tsx o bdlo fih</Text>
+      <Text>testing jsx</Text>
+      <Text>jsx working ?</Text>
+    </View>
+  );
+}
