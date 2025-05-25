d4addd2081c4da24ef6c50d71382b90f3b58d7d8
BouzirJAwad <jawadbouzir01@gmail.com>
2025-05-25 13:28:16 +0100

    re-adding native file

--- native/app/index.tsx
+++ native/app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-red-600 font-bold">nativewind test</Text>
-      <Text className="text-5xl text-zre9-3"> Hello World</Text>
+      <Text className="text-red-600 text-4xl">hello world</Text>
+      <Text className="text-2xl">daba siro l app/index.tsx o bdlo fih</Text>
     </View>
   );
 }
--- native/tailwind.config.js
+++ native/tailwind.config.js
@@ -4,16 +4,7 @@ module.exports = {
   content: ["./App/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        lbyed: "#F8F6F3", 
-        zre9: {
-          1: "#89AFD2",
-          2: "#4299E1",
-          3: "#3D5681"
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
